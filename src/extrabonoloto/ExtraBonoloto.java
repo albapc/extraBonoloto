6c35bee39e0a7d9216fefd84b6a788065a6d946e
albapc <alba.perez.ce@gmail.com>
2019-04-18 19:29:45 +0200

    commit final

---
+++ src/extrabonoloto/ExtraBonoloto.java
@@ -0,0 +1,11 @@
+
+package extrabonoloto;
+
+public class ExtraBonoloto {
+
+    public static void main(String[] args) {
+        Loteria lot = new Loteria();
+        lot.setVisible(true);
+    }
+    
+}
